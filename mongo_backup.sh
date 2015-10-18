a652fa53fed1ed48a33450af69da7f5fa7f59346
rajesh.gupta <rajesh.gupta@olacabs.com>
2015-10-18 19:40:47 +0530

    mongo_backup.sh

---
+++ mongo_backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Purpose To take dump of  database1 and database2 database backup
+#Author Devops Team (Rajesh)
+#Lats updated  18 oct 2015
+
+DUMPDIR=/usr/share/org/mongo_dump
+ARCHIVEDIR=/usr/share/org/mongo_dump/archive
+
+DB1=database1
+DB2=database2
+
+cd /usr/share/org/mongo_dump
+for i in ${DB1} ${DB2}
+	do
+	mongodump --db $i --out ${DUMPDIR}
+	logger "Created dump of $i in  ${DUMPDIR}"
+	tar -P --force-local -cvzf  $DUMPDIR/$i-$(date +%F-%H-%M-%S).tar.gz  -C $DUMPDIR $i 
+	s3cmd put ${DUMPDIR}/${i}-*.gz	s3://org-dbbackup/mongo/
+	logger "uploaded ${i} dump to s3://org-dbbackup/mongo/"
+	done
+mv $DUMPDIR/*.gz $ARCHIVEDIR/
