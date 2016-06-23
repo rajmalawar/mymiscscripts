9a9bcc4d364eaf7326f2ef71220e07e445769ba4
rajesh gupta <rajesh.gupta@olacabs.com>
2016-06-24 00:15:41 +0530

    added sensu-check folder

---
+++ sensu-checks/marathonapphealthcheck.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+#Sensu check for Marathon app running or not
+require "net/http"
+require "uri"
+require "json"
+require 'sensu-plugin/check/cli'
+class MarathonAppHealthCheck   < Sensu::Plugin::Check::CLI
+  option :id,
+         description: 'applicaton id',
+         short: '-i Marathon app ID',
+         long: '--id Marathon app ID',
+         required: true
+  option :server,
+         description: 'Marathon hosts, comma separated',
+         short: '-s SERVER',
+         long: '--server SERVER',
+         default: 'localhost'
+     option :port,
+         description: 'Marathon port',
+         short: '-p PORT',
+         long: '--port PORT',
+         default: '8080'
+def run
+
+uri = URI.parse("http://#{config[:server]}:#{config[:port]}/v2/apps/#{config[:id]}/tasks")
+response = Net::HTTP.get_response(uri)
+data_hash = JSON.parse(response.body)
+ if data_hash['tasks'][0].nil?
+  critical "NO task running for #{config[:id]}. Please check"
+ end
+data_hash['tasks'].each do |health|
+  health['healthCheckResults'].each do |live|
+        alive = live['alive']
+#If True        
+     if alive == true
+     ok "#{config[:id]} running healthy"
+#If blank
+
+#If false
+        elsif alive == false
+        critical "#{config[:id]} NOT running, Health Check failed. Please check"
+        else
+        unknown "kindly check, exit status is unknown"
+        end
+
+#end of both loops
+end
+end
+#end of class, run
+end
+end
