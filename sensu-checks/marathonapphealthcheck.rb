e572e315f5787e70f48d376f24b1c8eafe634dcf
Rajesh gupta <rajmalawar@gmail.com>
2016-06-24 00:17:40 +0530

    Update marathonapphealthcheck.rb

--- sensu-checks/marathonapphealthcheck.rb
+++ sensu-checks/marathonapphealthcheck.rb
@@ -25,23 +25,25 @@ def run
 uri = URI.parse("http://#{config[:server]}:#{config[:port]}/v2/apps/#{config[:id]}/tasks")
 response = Net::HTTP.get_response(uri)
 data_hash = JSON.parse(response.body)
+
+#If No tasks are running
  if data_hash['tasks'][0].nil?
-  critical "NO task running for #{config[:id]}. Please check"
+  critical "NO task running for #{config[:id]}. Please check"	
  end
+
 data_hash['tasks'].each do |health|
   health['healthCheckResults'].each do |live|
-        alive = live['alive']
-#If True        
+	alive = live['alive']
+#If True	
      if alive == true
-     ok "#{config[:id]} running healthy"
-#If blank
-
+     ok "#{config[:id]} running healthy"	
+      
 #If false
-        elsif alive == false
-        critical "#{config[:id]} NOT running, Health Check failed. Please check"
-        else
-        unknown "kindly check, exit status is unknown"
-        end
+	elsif alive == false
+	critical "#{config[:id]} NOT running, Health Check failed. Please check"
+	else 
+	unknown "kindly check, exit status is unknown"
+	end
 
 #end of both loops
 end
